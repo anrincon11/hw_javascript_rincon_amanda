15b33d9d1615ac11f96451f97bbce199cba7e122
Amanda Rincon <anrincon@ufl.edu>
2016-02-04 13:04:09 -0800

    add a function declaration on the functions branch

--- js/main.js
+++ js/main.js
@@ -2,11 +2,23 @@ var iceCream = [ 'Rocky Road', 'Chocolate Chip', 'Lemon Sorbet',
 	'Vanilla' ];
 
 var presidents = [
-		{ firstName: 'Barack', lastName: 'Obama', termLength: 2, party: 'Democratic', yearsOfPresidency: 8 },
-		{ firstName: 'George', lastName: 'Bush', termLength: 2, party: 'Republican', yearsOfPresidency: 8 },
-		{ firstName: 'Bill', lastName: 'Clinton', termLength: 2, party: 'Democratic', yearsOfPresidency: 8 },
-		{ firstName: 'George', lastName: 'Bush', termLength: 1, party: 'Republican', yearsOfPresidency: 4 },
-		{ firstName: 'Ronald', lastName: 'Reagan', termLength: 2, party: 'Republican', yearsOfPresidency: 8 }
+		{ firstName: 'Barack', lastName: 'Obama',
+			termLength: 2, party: 'Democratic', yearsOfPresidency: 8 },
+		{ firstName: 'George', lastName: 'Bush',
+			termLength: 2, party: 'Republican', yearsOfPresidency: 8 },
+		{ firstName: 'Bill', lastName: 'Clinton',
+			termLength: 2, party: 'Democratic', yearsOfPresidency: 8 },
+		{ firstName: 'George', lastName: 'Bush',
+			termLength: 1, party: 'Republican', yearsOfPresidency: 4 },
+		{ firstName: 'Ronald', lastName: 'Reagan',
+			termLength: 2, party: 'Republican', yearsOfPresidency: 8 }
 	];
 
-console.log( presidents[2].firstName, presidents[2].lastName );
+console.log( presidents[ 2 ].firstName, presidents[ 2 ].lastName );
+
+function howdy ( person, message ) {
+	return person + 'says' + message;
+}
+
+var greeting = combiner( 'Amanda Rincon', 'Hi!' );
+console.log( message );
